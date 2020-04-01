aa72d04bfe1435cacef2eb089e44c5d40147f21c
Vanessa Chu <vchu22@users.noreply.github.com>
2020-04-01 11:02:31 -0400

    added chart for historical data

--- src/js/components/Display.js
+++ src/js/components/Display.js
@@ -1,6 +1,15 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import styled from 'styled-components';
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from 'recharts';
 
 const DataDisplay = styled.div`
   font-size: 1.5em;
@@ -24,36 +33,26 @@ class Display extends Component {
   constructor() {
     super();
     this.state = {
-      histCases: [],
-      histDeaths: [],
-      histRecovered: [],
+      histData: [],
     };
   }
 
   componentDidMount() {
-    // console.log(this.props.details.country);
+    console.log(this.props.details.country);
     axios
       .get(
         `https://corona.lmao.ninja/v2/historical/${this.props.details.country}`
       )
       .then(res => {
         const { cases, deaths, recovered } = res.data.timeline;
-        let c = [],
-          d = [],
-          r = [];
-        for (let [key, value] of Object.entries(cases)) {
-          c.push({ date: key, number: value });
-        }
-        for (let [key, value] of Object.entries(deaths)) {
-          d.push({ date: key, number: value });
-        }
-        for (let [key, value] of Object.entries(recovered)) {
-          r.push({ date: key, number: value });
+        let histData = [];
+        for (let [date, c] of Object.entries(cases)) {
+          const d = deaths[date];
+          const r = recovered[date];
+          histData.push({ date, cases: c, deaths: d, recovered: r });
         }
         this.setState({
-          histCases: c,
-          histDeaths: d,
-          histRecovered: r,
+          histData,
         });
       });
   }
@@ -85,6 +84,42 @@ class Display extends Component {
         <div>Critical: {format(critical)}</div>
         <div>Recovery: {format(recovered)}</div>
         <div>Active: {format(active)}</div>
+        <h4>Historical Data</h4>
+        <LineChart
+          width={1000}
+          height={500}
+          data={this.state.histData}
+          margin={{
+            top: 5,
+            right: 30,
+            left: 50,
+            bottom: 5,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis dataKey="date" hide={true} />
+          <YAxis />
+          <Tooltip />
+          <Legend />
+          <Line
+            type="monotone"
+            dataKey="cases"
+            stroke="#6A5ACD"
+            activeDot={{ r: 6 }}
+          />
+          <Line
+            type="monotone"
+            dataKey="deaths"
+            stroke="#191970"
+            activeDot={{ r: 8 }}
+          />
+          <Line
+            type="monotone"
+            dataKey="recovered"
+            stroke="#3CB371"
+            activeDot={{ r: 8 }}
+          />
+        </LineChart>
       </DataDisplay>
     );
   }
