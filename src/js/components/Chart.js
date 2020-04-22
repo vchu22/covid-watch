79abfd4e326b5aaddf68389d1300ea991e86318f
Vanessa Chu <vchu22@users.noreply.github.com>
2020-04-21 22:07:35 -0400

    moved chart into a separate component

---
+++ src/js/components/Chart.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import DataStore from '../store';
+import {
+  ResponsiveContainer,
+  AreaChart,
+  Area,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+} from 'recharts';
+
+class Chart extends Component {
+  constructor() {
+    super();
+    this.state = {
+      histData: DataStore.getHistoricalData(),
+    };
+  }
+  componentDidMount() {
+    DataStore.on('change', () => {
+      this.setState({
+        histData: DataStore.getHistoricalData(),
+      });
+    });
+  }
+  render() {
+    return this.state.histData ? (
+      <div>
+        <h4>Historical Data</h4>
+        <ResponsiveContainer width="100%" height={500}>
+          <AreaChart
+            data={this.state.histData}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 50,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="date" hide={true} />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Area
+              type="monotone"
+              dataKey="cases"
+              stroke="#6A5ACD"
+              fill="#6A5ACD"
+              activeDot={{ r: 10 }}
+            />
+            <Area
+              type="monotone"
+              dataKey="recovered"
+              stroke="#00e600"
+              fill="#00e600"
+              activeDot={{ r: 8 }}
+            />
+            <Area
+              type="monotone"
+              dataKey="deaths"
+              stroke="#FF3300"
+              fill="#FF3300"
+              activeDot={{ r: 5 }}
+            />
+          </AreaChart>
+        </ResponsiveContainer>
+      </div>
+    ) : null;
+  }
+}
+export default Chart;
--- src/js/components/Display.js
+++ src/js/components/Display.js
@@ -2,17 +2,8 @@ import React, { Component } from 'react';
 import DataStore from '../store';
 import { fetchHistoricalData } from '../actions';
 import styled from 'styled-components';
-import {
-  ResponsiveContainer,
-  AreaChart,
-  Area,
-  XAxis,
-  YAxis,
-  CartesianGrid,
-  Tooltip,
-  Legend,
-} from 'recharts';
 import Card from './Card';
+import Chart from './Chart';
 
 const DataDisplay = styled.div`
   font-size: 1.5em;
@@ -41,7 +32,6 @@ class Display extends Component {
     this.state = {
       selectedCountry: DataStore.getSelectedCountry(),
       countriesData: DataStore.getCountriesData(),
-      histData: DataStore.getHistoricalData(),
     };
   }
 
@@ -51,7 +41,6 @@ class Display extends Component {
       this.setState({
         selectedCountry: DataStore.getSelectedCountry(),
         countriesData: DataStore.getCountriesData(),
-        histData: DataStore.getHistoricalData(),
       });
     });
   }
@@ -90,45 +79,8 @@ class Display extends Component {
         <Card title="Critical" number={critical} color="#6600cc"></Card>
         <Card title="Recovery" number={recovered} color="#33cc33"></Card>
         <Card title="Active" number={active} color="#0099ff"></Card>
-        <h4>Historical Data</h4>
-        <ResponsiveContainer width="100%" height={500}>
-          <AreaChart
-            data={this.state.histData}
-            margin={{
-              top: 5,
-              right: 30,
-              left: 50,
-              bottom: 5,
-            }}
-          >
-            <CartesianGrid strokeDasharray="3 3" />
-            <XAxis dataKey="date" hide={true} />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Area
-              type="monotone"
-              dataKey="cases"
-              stroke="#6A5ACD"
-              fill="#6A5ACD"
-              activeDot={{ r: 10 }}
-            />
-            <Area
-              type="monotone"
-              dataKey="recovered"
-              stroke="#00e600"
-              fill="#00e600"
-              activeDot={{ r: 8 }}
-            />
-            <Area
-              type="monotone"
-              dataKey="deaths"
-              stroke="#FF3300"
-              fill="#FF3300"
-              activeDot={{ r: 5 }}
-            />
-          </AreaChart>
-        </ResponsiveContainer>
+
+        <Chart />
       </DataDisplay>
     ) : null;
   }
